117ae37c870c803ea2dfcc8d149709d740e0f4e1
Eliesser Freites <freiteseliesser@hotmail.com>
2025-05-01 12:58:32 +0200

    feat: Adds custom font loading and defines new colours in the Colors file. Improves the layout design and updates the CalculatorApp component with custom styles.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,14 +1,21 @@
 import { Slot } from 'expo-router';
 import { View, Text } from 'react-native';
+import { useFonts } from 'expo-font';
+import { Colors } from '@/constants/Colors';
+import { StatusBar } from 'expo-status-bar';
 
 const RootLayout = () => {
-  return (
-    <View>
-      <Text>Header</Text>
+  const [loaded] = useFonts({
+    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+  })
 
+  if (!loaded) return null;
+
+  return (
+    <View style={{ backgroundColor: Colors.background, flex: 1 }}>
       <Slot />
 
-      <Text>Footer</Text>
+      <StatusBar style="light" />
     </View>
   )
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,18 @@
+import { Colors } from '@/constants/Colors'
 import { View, Text } from 'react-native'
 
 const CalculatorApp = () => {
   return (
     <View>
-      <Text>CalculatorApp</Text>
+      <Text
+        style={{
+          fontSize: 40,
+          fontFamily: 'SpaceMono',
+          color: Colors.textPrimary,
+        }}
+      >
+        CalculatorApp
+      </Text>
     </View>
   )
 }
