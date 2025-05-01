f528da59271423eb8ee51081949f50e8045117b4
Eliesser Freites <freiteseliesser@hotmail.com>
2025-05-01 14:24:14 +0200

    feat:Add the expo-navigation-bar dependency and set the background colour of the navigation bar in Android. Update package.json and pnpm-lock.yaml to include the new dependency.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,10 @@
-import { View, Text } from 'react-native'
+import { View, Platform } from 'react-native'
+import { useEffect } from 'react'
 
 import { Slot } from 'expo-router'
 import { useFonts } from 'expo-font'
 import { StatusBar } from 'expo-status-bar'
+import * as NavigationBar from 'expo-navigation-bar'
 
 import { globalStyles } from '@/styles/global-styles'
 
@@ -10,6 +12,15 @@ const RootLayout = () => {
   const [loaded] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
   })
+
+  useEffect(() => {
+    if (Platform.OS === 'android') {
+      NavigationBar.setBackgroundColorAsync('black')
+        .catch(() => {
+          // Mute the error as it is expected on iOS
+        })
+    }
+  }, [])
 
   if (!loaded) return null;
 
