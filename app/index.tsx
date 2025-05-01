6b03d01445342e669b14fd8f09eefca1e922019e
Eliesser Freites <freiteseliesser@hotmail.com>
2025-05-01 14:51:01 +0200

    refactor: Updated the button logic in the CalculatorApp to build numbers using the buildNumber function instead of printing to the console. Improved the buildNumber function in the useCalculator hook to correctly handle number input.

--- app/index.tsx
+++ app/index.tsx
@@ -24,23 +24,23 @@ const CalculatorApp = () => {
       </View>
 
       <View style={globalStyles.row}>
-        <CalculatorButton label='7' onPress={() => { console.log('7') }} />
-        <CalculatorButton label='8' onPress={() => { console.log('8') }} />
-        <CalculatorButton label='9' onPress={() => { console.log('9') }} />
+        <CalculatorButton label='7' onPress={() => { buildNumber('7') }} />
+        <CalculatorButton label='8' onPress={() => { buildNumber('8') }} />
+        <CalculatorButton label='9' onPress={() => { buildNumber('9') }} />
         <CalculatorButton label='X' onPress={() => { console.log('X') }} color={Colors.orange} />
       </View>
 
       <View style={globalStyles.row}>
-        <CalculatorButton label='4' onPress={() => { console.log('4') }} />
-        <CalculatorButton label='5' onPress={() => { console.log('5') }} />
-        <CalculatorButton label='6' onPress={() => { console.log('6') }} />
+        <CalculatorButton label='4' onPress={() => { buildNumber('4') }} />
+        <CalculatorButton label='5' onPress={() => { buildNumber('5') }} />
+        <CalculatorButton label='6' onPress={() => { buildNumber('6') }} />
         <CalculatorButton label='-' onPress={() => { console.log('-') }} color={Colors.orange} />
       </View>
 
       <View style={globalStyles.row}>
-        <CalculatorButton label='1' onPress={() => { console.log('1') }} />
-        <CalculatorButton label='2' onPress={() => { console.log('2') }} />
-        <CalculatorButton label='3' onPress={() => { console.log('3') }} />
+        <CalculatorButton label='1' onPress={() => { buildNumber('1') }} />
+        <CalculatorButton label='2' onPress={() => { buildNumber('2') }} />
+        <CalculatorButton label='3' onPress={() => { buildNumber('3') }} />
         <CalculatorButton label='+' onPress={() => { console.log('+') }} color={Colors.orange} />
       </View>
 
